1d4ca397c1f59e9c7a59f4b242eb5ebe16e8b28c
aashikur <mdaashikur@gmail.com>
2025-04-13 14:58:12 +0600

    ReCrated the Function One more TIme

--- src/App.jsx
+++ src/App.jsx
@@ -1,70 +1,9 @@
-// import React, { useEffect, useState } from 'react';
-// import 'react-toastify/dist/ReactToastify.css';
 import { useEffect, useState } from 'react';
 import Test from './components/Test';
-import { FaV } from 'react-icons/fa6';
-
+import Footer from './components/Footer/Footer'
 function App() {  
-  // const [TotalPrice, setTotalPrice] = useState(0);
-  // const [AuctionData, setAuctionData] = useState([]);
-  // const [FavoriteList, setFavoriteList] = useState([]);
-
-  // console.log('upper',AuctionData);
-
-  // useEffect(() => {
-  //   fetch('/auction-data.json')
-  //     .then(res => res.json())
-  //     .then(data => {
-  //       const updatedData = data.map(item => ({ ...item, isFavorite: false }));
-  //       setAuctionData(updatedData);
-  //     });
-  // }, []);
-  // // console.log(AuctionData);
-  // const calculateTotalPrice = (favorites) => {
-  //   const total = favorites.reduce((sum, item) => sum + parseFloat(item.currentBidPrice || 0), 0);
-  //   setTotalPrice(total);
-  // };
-
-  // const handleFavorite = (id) => {
-  //   const updatedData = AuctionData.map(item => {
-  //     if (item.id === id) {
-  //       item.isFavorite = true; 
-  //     }
-  //     return item;
-  //   });
-  //   setAuctionData(updatedData);
-
-  //   const newFav = AuctionData.find(item => item.id === id);
-  //   const updatedFavorites = [...FavoriteList, newFav];
-  //   setFavoriteList(updatedFavorites);
-  //   calculateTotalPrice(updatedFavorites);
-  // };
-
-  // const removeList = (id) => {
-  //   const updatedData = AuctionData.map(item => {
-  //     if (item.id === id) {
-  //       item.isFavorite = false; 
-  //     }
-  //     return item;
-  //   });
-  //   setAuctionData(updatedData);
-
-  //   const updatedFavorites = FavoriteList.filter(item => item.id !== id);
-  //   setFavoriteList(updatedFavorites);
-  //   calculateTotalPrice(updatedFavorites);
-  // };
-// ===============================================================================================================================================================
-  // {
-  //   "id": 2,
-  //   "title": "Antique Pocket Watch",
-  //   "description": "Classic mechanical pocket watch from the early 1900s.",
-  //   "currentBidPrice": 310.00,
-  //   "timeLeft": "5h 10m",
-  //   "bidsCount": 22,
-  //   "image": "https://i.ibb.co.com/nNC17Syw/Bid2-min.png"
-  // },
-
-const FavRemove = (id) => {
+ 
+const FavRemove = (id) => { 
   console.log(id) 
   const updateFavList = FavoriteList.filter(item => item.id !== id)
   setFavoriteList(updateFavList)
@@ -82,7 +21,6 @@ const FavRemove = (id) => {
 }
 
   const heartButton = (id) => { 
-    // console.log('heart button',id) 
 
  
     const updateData = AuctionData.map(item => {
@@ -129,23 +67,14 @@ const FavRemove = (id) => {
 
     setTotalPrice(sum)
   }
-  
-  // FavoriteList.map(item => console.log(item.title))
-
-  // useEffect(()=>{
-  //   console.log(AuctionData[0])
-  // },[AuctionData])
 
   return (
     <>
-      {/* <div className='sticky w-2/5 mx-auto  top-0 bg-red-950 text-center py-y'> 
-      {
-        FavoriteList.map(item=><p>{item.title}</p>)
-      }
-      </div> */}
+
       
       <Test FavRemove={FavRemove}  FavoriteList={FavoriteList} TotalPrice={TotalPrice} heartButton={heartButton} AuctionData={AuctionData}></Test>
 
+    <Footer></Footer>
     </>
   );
 }
